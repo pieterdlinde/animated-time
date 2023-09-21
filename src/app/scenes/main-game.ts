d50ac0a6ec9a8a303002ac2793eee8f58d03bd4b
Pieter Linde <Pieter.Linde@rr-wfm.com>
2023-09-21 09:11:06 +0200

    Update main Game

--- src/app/scenes/main-game.ts
+++ src/app/scenes/main-game.ts
@@ -6,7 +6,7 @@ import { PlayerSetup, Players } from "../resources/players";
 
 export default class MainGame extends Scene {
     private gridEngine!: GridEngineExtender;
-
+    private isShowingTime = false;
     constructor() {
         super('maingame')
     }
@@ -51,22 +51,35 @@ export default class MainGame extends Scene {
  
         
         this.gridEngine.create(mainMap, gridEngineConfig);
-        for (let index = 1; index < 60; index++) {
-             
-            this.gridEngine.moveRandomly("dummy"+index, this.getRandomInt(100,1500), -1)
-        }  
 
     }
-
+    
     update() {
         PlayerSetup.UpdatePlayerBasicKeyboardMovement(this, this.gridEngine);
         const cursor = this.input.keyboard?.createCursorKeys();
 
         // Test Move to
-        if (cursor?.space.isDown) {
-            this.gridEngine.moveTo(Players.Hero.name, {
-                x: 8, y: 8
-            })
+
+        if (cursor?.space.isUp) {
+            this.isShowingTime = !this.isShowingTime;
+        }
+
+        if (cursor?.space.isDown) { 
+        
+
+            if(this.isShowingTime){
+                for (let index = 1; index < 60; index++) { 
+                    this.gridEngine.moveTo("dummy"+index, {
+                        x: 15, y: 15
+                    })
+                }  
+                console.log("IF")
+            }else{
+                for (let index = 1; index < 60; index++) { 
+                    this.gridEngine.moveRandomly("dummy"+index, this.getRandomInt(100,1500), -1)
+                }   
+                console.log("Else")
+            }  
         }
 
         if (cursor?.shift.isDown) {
